8510155c927ac79bac941079bab39c6c89bd896d
anandhu-as <anandhuas010203@gmail.com>
2024-01-08 22:04:40 +0530

    first commit

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,21 +1,17 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from 'react'
+
 const App = () => {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const handleClick = () => setTodos([...todo]);
+  const [count,setCount]=useState(0);
   return (
     <div>
-      <h3>Todo App</h3>
-      <input type="text" onChange={(e) => setTodo(e.target.value)} />
-      <button onClick={() => handleClick()}>add</button>
-      <h1>
-        {todos.map((data) => {
-          return <>{data}</>;
-        })}
-      </h1>
+      <h1>Counter</h1>
+      <button>+</button>
+      <h1>{count}</h1>
+      <button>-</button>
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
